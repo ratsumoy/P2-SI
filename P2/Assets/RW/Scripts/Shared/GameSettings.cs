65b59563e925dc03cefb40d8ade7e96f88246b74
ratsumoy <92041105+ratsumoy@users.noreply.github.com>
2024-04-29 09:01:42 +0200

    P2

---
+++ P2/Assets/RW/Scripts/Shared/GameSettings.cs
@@ -0,0 +1,4 @@
+﻿public static class GameSettings
+{
+    public static HayMachineColor hayMachineColor = HayMachineColor.Blue;
+}
---
+++ P2/Assets/RW/Scripts/Shared/HayMachineColor.cs
@@ -0,0 +1,5 @@
+﻿
+public enum HayMachineColor
+{
+    Blue, Yellow, Red
+}
